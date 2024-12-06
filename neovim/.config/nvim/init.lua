c2fb9dfdfc1afb80c488cf3544de69f1b16e3372
Hayden Foley <hpfoley1@gmail.com>
2024-12-06 18:35:06 -0500

    Added neovim

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,16 @@
+vim.opt.background = "dark"
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.smartindent = true
+vim.opt.ignorecase = true
+vim.opt.scrolloff = 3
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+
+vim.opt.list = true
+vim.opt.listchars = {space = '·', tab = '»·' }
+vim.cmd('highlight Whitespace guifg=#4a7a4a')
+
