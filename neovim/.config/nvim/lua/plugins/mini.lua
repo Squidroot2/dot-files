2f3b301793b325b7551a8770cccd77d4d8e75778
Hayden Foley <hpfoley1@gmail.com>
2024-12-15 12:00:12 -0500

    Added mini files

--- neovim/.config/nvim/lua/plugins/mini.lua
+++ neovim/.config/nvim/lua/plugins/mini.lua
@@ -4,6 +4,10 @@ return {
         version = false,
         config = function()
             require('mini.statusline').setup()
+            require('mini.icons').setup()
+            require('mini.files').setup()
+
+            vim.keymap.set("n", "<leader>mf", require('mini.files').open, { desc = "Open Mini File Explorer" })
         end
     },
 }
